bb682f65cebc9773a31bc6f2a61bfc6051b172c3
Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>
2016-06-13 15:11:37 +0200

    Added command-line parameter parsing
    Signed-off-by: Rutger Brouwer <r.w.w.brouwer@erasmusmc.nl>

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 
 // BOOST libraries
+#include <boost/program_options.hpp>
 
 // own libraries
 #include <sequence_matcher.hpp>
@@ -81,23 +82,81 @@ int process_reads(std::istream& hin, std::ostream& hout, const std::vector< Biom
     return 0 ;
 }
 
-
 //
 // the main program loop
 //
 //
 int main(int argc, char** argv) {
+    
     //
     int return_code = 0 ;
+    std::istream hin ;
+    std::istream hout ;
+    
+    // the input parameters 
+    std::string file_input = "-" ;
+    std::string file_output = "-" ;
+    std::string file_adapters = "-" ;
+    std::vector<std::string> adapter_sequences(0) ;
+    std::size_t maximum_mismatches = 2 ;
+    std::size_t minimum_matches = 1 ;
+    std::size_t minimum_bases_remaining = 25 ;    
+    std::size_t buffer_size = 1000 ;
+        
+    // parse the command-line parameters
+    boost::program_options::options_description desc("Allowed options") ;
+    desc.add_options()
+        ("help", "Produce the help message")
+        ("input,i", boost::program_options::value<std::string>(&file_input), "The input FastQ file")
+        ("output,o", boost::program_options::value<std::string>(&file_output), "The output FastQ file")
+        ("adapter,a", boost::program_options::value< std::vector<std::string> >(&adapter_sequences), "The adapter sequence to trim")
+        ("adapter-file,f", boost::program_options::value<std::string>(&file_adapters), "A file with adapter sequences")
+        ("maximum-mismatches", boost::program_options::value<std::size_t>(&maximum_mismatches), "The maximum number of mismatches")
+        ("minimum-matches", boost::program_options::value<std::size_t>(&minimum_matches), "The minimum number of matching bases")
+        ("minimum-bases-remaining", boost::program_options::value<std::size_t>(&minimum_bases_remaining), "The minimum number of bases in the reads remaining")
+        ("buffer-size", boost::program_options::value<std::size_t>(&buffer_size), "The size of the read buffer to process") ;    
+    boost::program_options::positional_options_description p ;
+	boost::program_options::variables_map vm ;
+	boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).positional(p).run(), vm) ;
+	boost::program_options::notify(vm) ;
+    
+    // check the command-line arguments
+    if( vm.count("help") ) {
+		std::cerr << "Usage" << std::endl ; 
+		std::cerr << desc << std::endl; 
+		return 0 ;
+	}    
+    if(file_input == "-"){            
+        hin = std::cin ;
+    } else { 
+        hin = (std::istream) std::ifstream(file_input, std::ifstream::in) ;
+    }   
+    if(file_output == "-"){
+        hout = std::cout ;
+    } else {
+        hin = (std::ostream) std::ofstream(file_output, std::ifstream::out) ;
+    }
+    if(file_adapters != "-"){
+        
+    }      
     
     // initialize the adapters
     std::vector<Biomics::SequenceMatcher<rwwb::sequtils::base_t> > adapters ;
-    adapters.push_back(
-        Biomics::SequenceMatcher<rwwb::sequtils::base_t>(rwwb::sequtils::string_to_base("AGATCGGAAGAG"), 1, 1)
-        ) ;
+    for(std::size_t i=0; i<adapter_sequences.size(); ++i){
+        adapters.push_back(Biomics::SequenceMatcher<rwwb::sequtils::base_t>(rwwb::sequtils::string_to_base(adapter_sequences[i]), maximum_mismatches, minimum_matches)) ;    
+    }        
     
-    // 
-    return_code = process_reads(std::cin, std::cout, adapters, 1000, 25) ;
+    // process the reads from standard in 
+    return_code = process_reads(hin, hout, adapters, buffer_size, minimum_bases_remaining) ;
+      
+      
+    // cleanup the opened files
+    if(file_input == "-"){
+        hin.close() ;
+    }
+    if(file_output != "-"){
+        hout.close() ;
+    }
       
     //
     return return_code ;
